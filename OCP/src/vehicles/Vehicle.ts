b3ea2bb9ee99753702983a5a6f004159cf0be092
Aldry Albuquerque <albuquerquealdry>
2023-12-05 08:39:36 -0300

    Adicionar ocp

---
+++ OCP/src/vehicles/Vehicle.ts
@@ -0,0 +1,11 @@
+export default class vehicle {
+    constructor(
+        private color: string,
+        private year: number,
+        private engine: number,
+        private seats: number,
+        private doors: number
+    ){
+
+    }
+}
