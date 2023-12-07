d578fedf6a7d2d967ef6bc9a4cb08f481631c1f4
Aldry Albuquerque <albuquerquealdry>
2023-12-07 09:51:02 -0300

    Adionar principio de liskov]

---
+++ 02-l-liskov-substitution-principle/src/index.ts
@@ -0,0 +1,9 @@
+import CreditCard from "./CreditCard";
+import DebitCard from "./DebitCard";
+import NubankRewards from "./NubankRewards";
+
+const card = new CreditCard();
+
+
+card.validate();
+card.collectPayment();
